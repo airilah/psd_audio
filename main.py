fbbfdf171fbb7de8185fe56bb2c20ae91ef50920
airilah <111733158+airilah@users.noreply.github.com>
2023-11-27 11:29:40 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,72 @@
+import pickle
+import pandas as pd
+import streamlit as st
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+st.markdown(
+    "<h1 style='text-align: center;'>Klasifikasi Kanker Payudara Menggunakan Model Random Forest</h1>", unsafe_allow_html=True
+)
+st.markdown(
+    "<h4 style='text-align: center;'>Anas Khoiri Abdillah | 210411100025 | PSD - B</h4>", unsafe_allow_html=True
+)
+
+# st.info("Data diperoleh dari situs UCI Machine Learning dan dapat diakses pada link berikut : https://archive.ics.uci.edu/dataset/891/cdc+diabetes+health+indicators ")
+
+# load dataset -------------------------------------------------------------------
+# split dataset menjadi data training dan data testing ---------------------------
+dataset_baru = pd.read_excel('c:/KULIAH/KULIAH SEMESTER 5/Proyek Saint Data (PSD)/penugasan/dataset_baru.xlsx')
+
+# memisahkan kolom fitur dan target
+fitur = dataset_baru.drop(columns=['Classification'], axis=1)
+target = dataset_baru['Classification']
+
+# melakukan pembagian dataset, dataset dibagi menjadi 80% data training dan 20% data testing
+fitur_train, fitur_test, target_train, target_test = train_test_split(fitur, target, test_size = 0.2, random_state=42)
+
+# normalisasi dataset ------------------------------------------------------------
+
+with open('c:/KULIAH/KULIAH SEMESTER 5/Proyek Saint Data (PSD)/penugasan/zscorescaler_baru.pkl', 'rb') as file_normalisasi:
+    zscore_scaler = pickle.load(file_normalisasi)
+    
+# menerapkan normalisasi zscore pada data training
+zscore_training = zscore_scaler.transform(fitur_train)
+
+# menerapkan normalisasi zscore pada data testing
+zscore_testing = zscore_scaler.transform(fitur_test)
+
+# implementasi data pda model
+with open('c:/KULIAH/KULIAH SEMESTER 5/Proyek Saint Data (PSD)/penugasan/gridrandomforestzscore.pkl', 'rb') as file_model:
+    model_rf = pickle.load(file_model)
+    
+model_rf.fit(zscore_training, target_train)
+
+st.warning("Masukkan hasil test Anda!")
+Age = st.number_input ('Input Umur Anda! ')
+Glucose = st.number_input ('Input hasil test Glukosa Anda!')
+Homa = st.number_input ('Input hasil test Homa Anda!')
+Leptin = st.number_input ('Input hasil test Leptin Anda!')
+Resistin = st.number_input ('Input hasil test Resistin Anda!')
+
+
+if st.button('Cek Status'):
+    if Age is not None and Glucose is not None and Homa is not None and Leptin is not None and Resistin is not None:
+        
+        Age = int(Age)
+        Glucose = float(Glucose)
+        Homa = float(Homa)
+        Leptin = float(Leptin)
+        Resistin = float(Resistin)
+        
+        # Prediksi berdasarkan input yang telah diubah menjadi numerik
+        prediksi = model_rf.predict([[Age, Glucose, Homa, Leptin, Resistin]])
+        st.text( prediksi[0])
+        if prediksi[0] == 1:
+            st.success("Anda diprediksi Kanker Jinak / belum terdiagnosis Kanker Payudara!")
+        if prediksi[0] == 2:
+            st.error("Anda diprediksi Kanker Ganas / terdiagnosis Kanker Payudara")
+        else:
+            st.error("Zonk")
+    else:
+        st.text('Data tidak boleh kosong. Harap isi semua kolom.')
+
+
